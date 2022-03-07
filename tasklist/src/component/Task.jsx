90ad8295e53477cd4ee0d521d5072beff62a2111
Rajinder Tpss <rajindersainitpss@gmail.com>
2022-03-07 15:41:17 +0530

    Tasklist

---
+++ tasklist/src/component/Task.jsx
@@ -0,0 +1,17 @@
+import React,{useState} from 'react';
+import TaskForm from './TaskForm';
+
+function Task() { 
+
+    const [edit,setEdit] = useState({
+        id: null,
+        value: '',
+    });
+
+
+    return tasks.map((task,index) =>{
+        <></>
+    })
+}
+
+export default Task;
---
+++ tasklist/src/component/TaskForm.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+
+function TaskForm(props) {
+    const [input, setInput] = useState("");
+
+    const handleInput = e => { setInput(e.target.value) }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        props.onSubmit({
+            id: Math.floor(Math.random() * 1000),
+            text: input,
+        });
+
+        setInput("");
+
+    }
+
+    return (
+        <section className="task_outer">
+            <form onSubmit={handleSubmit}>
+                <div className="form_field">
+                    <input
+                        type="text"
+                        className="form_input"
+                        placeholder="Add Task Tittle"
+                        value={input}
+                        onChange={handleInput}
+                    />
+                    <button>Add Task</button>
+
+                </div>
+            </form>
+        </section>
+    )
+}
+
+export default TaskForm;
