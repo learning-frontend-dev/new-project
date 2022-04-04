ed954ed029677e408d81946ebfa895250a88fd57
Rajinder Tpss <rajindersainitpss@gmail.com>
2022-04-04 14:08:42 +0530

    routes

--- src/routers.jsx
+++ src/routers.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route} from 'react-router-dom';
+import { Routes, Route, useLocation} from 'react-router-dom';
 import Home from './component/pages/home/Home';
 import ContactUs from './component/pages/contact/ContactUs';
 import AboutUs from './component/pages/about/AboutUs';
@@ -7,9 +7,9 @@ import News from './component/pages/news/News';
 import Feature from './component/pages/feature/Feature';
 
 const Paths = () => {
-        
+    const history = useLocation();
     return (
-        <Routes initialEntries={['/home']}>
+        <Routes history={history}>
             <Route exact path="/home" element={<Home />} />
             <Route exact path="/contact-us" element={<ContactUs />} />
             <Route exact path="/news" element={<News />} />
